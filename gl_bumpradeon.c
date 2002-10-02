55f5701695a070ffa05c6a3ee2de95262c3a959a
jpaana <jpaana>
2002-10-02 17:45:24 +0000

    Use eye-z instead of proper distance as it's faster and what the fixed pipe is doing anyway...

--- gl_bumpradeon.c
+++ gl_bumpradeon.c
@@ -261,7 +261,7 @@ qboolean	GL_LookupRadeonSymbols (void)
 void GL_CreateShadersRadeon()
 {
     float scaler[4] = {0.5f, 0.5f, 0.5f, 0.5f};
-    GLuint mvp, projmatrix;
+    GLuint mvp, modelview, zcomp;
     GLuint texturematrix;
     GLuint vertex;
     GLuint texcoord0;
@@ -269,9 +269,8 @@ void GL_CreateShadersRadeon()
     GLuint texcoord2;
     GLuint color;
     GLuint supportedTmu;
-    GLuint disttemp, disttemp2, disttemp3;
+    GLuint disttemp, disttemp2;
     GLuint fogstart, fogend;
-    GLuint fogstart2, fogend2, zscaler, scaletemp;
 
 #if !defined(__APPLE__) && !defined (MACOSX)
     SAFE_GET_PROC( qglGenFragmentShadersATI, PFNGLGENFRAGMENTSHADERSATIPROC, "glGenFragmentShadersATI");
@@ -561,7 +560,7 @@ void GL_CreateShadersRadeon()
     // Generates a necessary input for the diffuse bumpmapping registers
     mvp           = qglBindParameterEXT( GL_MVP_MATRIX_EXT );
     checkerror();
-    projmatrix    = qglBindParameterEXT( GL_PROJECTION_MATRIX );
+    modelview     = qglBindParameterEXT( GL_MODELVIEW_MATRIX );
     checkerror();
     vertex        = qglBindParameterEXT( GL_CURRENT_VERTEX_EXT );
     checkerror();
@@ -575,29 +574,19 @@ void GL_CreateShadersRadeon()
     checkerror();
     texcoord2     = qglBindTextureUnitParameterEXT( GL_TEXTURE2_ARB, GL_CURRENT_TEXTURE_COORDS );
     checkerror();
-    disttemp      = qglGenSymbolsEXT(GL_VECTOR_EXT, GL_LOCAL_EXT, GL_FULL_RANGE_EXT, 1);
+    disttemp      = qglGenSymbolsEXT(GL_SCALAR_EXT, GL_LOCAL_EXT, GL_FULL_RANGE_EXT, 1);
     checkerror();
     disttemp2     = qglGenSymbolsEXT(GL_SCALAR_EXT, GL_LOCAL_EXT, GL_FULL_RANGE_EXT, 1);
     checkerror();
-    disttemp3     = qglGenSymbolsEXT(GL_SCALAR_EXT, GL_LOCAL_EXT, GL_FULL_RANGE_EXT, 1);
+    zcomp         = qglGenSymbolsEXT(GL_VECTOR_EXT, GL_LOCAL_EXT, GL_FULL_RANGE_EXT, 1);
     checkerror();
     fogstart      = qglBindParameterEXT( GL_FOG_START );
     checkerror();
     fogend        = qglBindParameterEXT( GL_FOG_END );
     checkerror();
-    fogstart2     = qglGenSymbolsEXT(GL_SCALAR_EXT, GL_LOCAL_EXT, GL_FULL_RANGE_EXT, 1);
-    checkerror();
-    fogend2       = qglGenSymbolsEXT(GL_SCALAR_EXT, GL_LOCAL_EXT, GL_FULL_RANGE_EXT, 1);
-    checkerror();
-    zscaler       = qglGenSymbolsEXT(GL_VECTOR_EXT, GL_LOCAL_EXT, GL_FULL_RANGE_EXT, 1);
-    checkerror();
-    scaletemp     = qglGenSymbolsEXT(GL_VECTOR_EXT, GL_LOCAL_EXT, GL_FULL_RANGE_EXT, 1);
-    checkerror();
 
     // Transform vertex to view-space
-    qglShaderOp2EXT( GL_OP_MULTIPLY_MATRIX_EXT, disttemp, mvp, vertex );
-    checkerror();
-    qglShaderOp1EXT( GL_OP_MOV_EXT, GL_OUTPUT_VERTEX_EXT, disttemp );
+    qglShaderOp2EXT( GL_OP_MULTIPLY_MATRIX_EXT, GL_OUTPUT_VERTEX_EXT, mvp, vertex );
     checkerror();
     
     // Transform vertex by texture matrix and copy to output
@@ -616,45 +605,22 @@ void GL_CreateShadersRadeon()
     qglShaderOp1EXT( GL_OP_MOV_EXT, GL_OUTPUT_COLOR0_EXT, color);
     checkerror();
 
-    // the fog values don't seem to be normalized to view, so we have to normalize them
-    // get z component from projection matrix
-    qglExtractComponentEXT( zscaler, projmatrix, 2);
+    // Transform vertex and take z for fog
+    qglExtractComponentEXT( zcomp, modelview, 2);
     checkerror();
-
-    // prepare { 0, 0, value, 1 } vector
-    qglSwizzleEXT(scaletemp, scaletemp, GL_ZERO_EXT, GL_ZERO_EXT, GL_ZERO_EXT, GL_ONE_EXT);
-    checkerror();
-
-    // first calculate scaled fog start
-    qglInsertComponentEXT(scaletemp, fogstart, 2);
-    checkerror();
-    qglShaderOp2EXT( GL_OP_DOT4_EXT, fogstart2, zscaler, scaletemp );
-    checkerror();
-
-    // then scaled fog end
-    qglInsertComponentEXT(scaletemp, fogend, 2);
-    checkerror();
-    qglShaderOp2EXT( GL_OP_DOT4_EXT, fogend2, zscaler, scaletemp );
-    checkerror();
-
-    // ok, now we have scaled fog parameters, calculate view distance
-    qglShaderOp2EXT( GL_OP_DOT3_EXT, disttemp2, disttemp, disttemp);
-    checkerror();
-    qglShaderOp1EXT( GL_OP_RECIP_SQRT_EXT, disttemp3, disttemp2);
-    checkerror();
-    qglShaderOp1EXT( GL_OP_RECIP_EXT, disttemp2, disttemp3);
+    qglShaderOp2EXT( GL_OP_DOT4_EXT, disttemp, zcomp, vertex );
     checkerror();
 
     // calculate fog values end - z and end - start
-    qglShaderOp2EXT( GL_OP_SUB_EXT, disttemp3, fogend2, disttemp2);
+    qglShaderOp2EXT( GL_OP_SUB_EXT, disttemp, fogend, disttemp);
     checkerror();
-    qglShaderOp2EXT( GL_OP_SUB_EXT, disttemp2, fogend2, fogstart2);
+    qglShaderOp2EXT( GL_OP_SUB_EXT, disttemp2, fogend, fogstart);
     checkerror();
 
     // divide end - z by end - start, that's it
     qglShaderOp1EXT( GL_OP_RECIP_EXT, disttemp2, disttemp2);
     checkerror();
-    qglShaderOp2EXT( GL_OP_MUL_EXT, GL_OUTPUT_FOG_EXT, disttemp2, disttemp3);
+    qglShaderOp2EXT( GL_OP_MUL_EXT, GL_OUTPUT_FOG_EXT, disttemp, disttemp2);
     checkerror();
 
     qglEndVertexShaderEXT();
