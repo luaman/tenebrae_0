b64e2687e9648aa1c52e83c99dbbb4f445b335f2
lorderadicator <lorderadicator>
2002-11-13 21:10:21 +0000

    sbar_updateperframe cvar to toggle wether HUD updates everyframe

--- cl_main.c
+++ cl_main.c
@@ -40,6 +40,8 @@ cvar_t	m_yaw = {"m_yaw","0.022", true};
 cvar_t	m_forward = {"m_forward","1", true};
 cvar_t	m_side = {"m_side","0.8", true};
 
+cvar_t	sbar_updateperframe = {"sbar_updateperframe","1", true};
+
 
 client_static_t	cls;
 client_state_t	cl;
@@ -964,6 +966,8 @@ void CL_Init (void)
 	Cvar_RegisterVariable (&m_yaw);
 	Cvar_RegisterVariable (&m_forward);
 	Cvar_RegisterVariable (&m_side);
+
+	Cvar_RegisterVariable (&sbar_updateperframe);
 
 //	Cvar_RegisterVariable (&cl_autofire);
 	
