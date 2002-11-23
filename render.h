bbfc1ba76126a12a76720cf9758a3d7097b4fc5f
cholleme <cholleme>
2002-11-23 19:18:39 +0000

    Added particle emitters

--- render.h
+++ render.h
@@ -149,6 +149,8 @@ void R_NewMap (void);
 
 
 void R_ParseParticleEffect (void);
+void R_ParseBasicEmitter (void);
+void R_ParseExtendedEmitter (void);
 void R_RunParticleEffect (vec3_t org, vec3_t dir, int color, int count);
 void R_RocketTrail (vec3_t start, vec3_t end, int type);
 
