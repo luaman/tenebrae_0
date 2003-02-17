2633ad7548600259025258e81727fee7d0a4b45b
lorderadicator <lorderadicator>
2003-02-17 03:02:05 +0000

    truly fixed blended font bug (added GL_BLEND just to be sure)

--- menu.c
+++ menu.c
@@ -201,6 +201,7 @@ Draws one solid graphics character
 */
 void M_DrawCharacter (int cx, int line, int num)
 {
+	glEnable(GL_BLEND);
 	Draw_Character ( cx + ((vid.width - 320)>>1), line, num);
 }
 
@@ -226,11 +227,13 @@ void M_PrintWhite (int cx, int cy, char *str)
 
 void M_DrawTransPic (int x, int y, qpic_t *pic)
 {
+	glEnable(GL_BLEND);
 	Draw_TransPic (x + ((vid.width - 320)>>1), y, pic);
 }
 
 void M_DrawPic (int x, int y, qpic_t *pic)
 {
+	glEnable(GL_BLEND);
 	Draw_Pic (x + ((vid.width - 320)>>1), y, pic);
 }
 
@@ -383,7 +386,6 @@ void M_Main_Draw (void)
 	int		f;
 	qpic_t	*p;
 
-	glEnable(GL_BLEND);
 
 	M_DrawTransPic (16, 4, Draw_CachePic ("gfx/qplaque.lmp") );
 	p = Draw_CachePic ("gfx/ttl_main.lmp");
