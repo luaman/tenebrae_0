6d67c5e5fd5f0e4bed68fa6b2a36e74dbe44c5da
jpaana <jpaana>
2003-11-30 01:42:21 +0000

    Linux patches from Shane Bayer and Industri team

--- common.c
+++ common.c
@@ -2052,6 +2052,8 @@ void COM_AddGameDirectory (char *dir)
 	char      *filename;        
 	dirdata_t dirdata;
 
+	Sys_mkdir(dir);
+
 // update current gamedir
 	strncpy (com_gamedir, dir, sizeof(com_gamedir));
 //
